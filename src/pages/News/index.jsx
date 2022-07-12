a31e6ca560d4bd6a0acac50616d702ee7b11fe86
icarobteles <eng.icaroteles@gmail.com>
2022-07-11 21:49:10 -0300

    feat(pages): :sparkles: adds conditional rendering
    Adds conditional rendering to determine the width of the container depending on whether the menu is expanded or not.

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import { useMenu } from "../../providers/menu";
 import { NewsContainer } from "./style";
 
 const NewsPage = () => {
+  const { menuIsExpanded } = useMenu();
   return (
-    <NewsContainer>
+    <NewsContainer menuIsExpanded={menuIsExpanded}>
       <Header namepage="Notícias" />
     </NewsContainer>
   );
--- src/pages/NotFound/style.js
+++ src/pages/NotFound/style.js
@@ -2,4 +2,10 @@ import styled from "styled-components";
 
 export const NotFoundContainer = styled.div`
   width: 100%;
+  transition: all ease 0.5s;
+  @media (min-width: 600px) {
+    margin-left: ${(props) => (props.menuIsExpanded ? "360px" : " 90px")};
+    width: ${(props) =>
+      props.menuIsExpanded ? "calc(100% - 360px)" : "calc(100% - 90px)"};
+  }
 `;
