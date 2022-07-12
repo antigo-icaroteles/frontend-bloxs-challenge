e1320a93b8d9b921bd1cfddcad75b9cad0e01314
icarobteles <eng.icaroteles@gmail.com>
2022-07-12 19:50:07 -0300

    feat(pages): :sparkles: edit NewsPage

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,12 +1,19 @@
+import Category from "../../components/Category";
 import Header from "../../components/Header";
 import { useMenu } from "../../providers/menu";
 import { NewsContainer } from "./style";
 
 const NewsPage = () => {
   const { menuIsExpanded } = useMenu();
+
   return (
     <NewsContainer menuIsExpanded={menuIsExpanded}>
       <Header namepage="Notícias" />
+
+      <main id="main" className="main">
+        <Category name="energy" />
+        <Category name="agribusiness" />
+      </main>
     </NewsContainer>
   );
 };
